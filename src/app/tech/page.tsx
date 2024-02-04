7672040fd84befba4347eba2bdb84a34d7e6bd3a
Samad-11 <120767334+Samad-11@users.noreply.github.com>
2024-02-04 18:46:33 +0530

    Update page title tech

--- src/app/tech/page.tsx
+++ src/app/tech/page.tsx
@@ -22,7 +22,7 @@ const page = async () => {
     let news: object[] = await data.articles;
 
     return (
-        <MainBody news={data.articles} heading={"Trending News"} />
+        <MainBody news={data.articles} heading={"Technology"} />
     )
 }
 
